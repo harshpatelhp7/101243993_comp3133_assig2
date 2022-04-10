3dcfd5df25c167ad2519e435baf3f19862890db8
Harsh Patel <harshpatelhp728@gmail.com>
2022-04-09 20:46:43 -0400

    initial folder upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'week12_angular_apollo_graphql';
+  listing_data = {};
+
+  private GET_LISTINGS = gql`
+    {
+      listings {
+        listing_title
+        city
+        description
+      }
+    }
+  `;
+
+  constructor(private apolloClient: Apollo) {
+    this.getListings();
+  }
+
+  getListings() {
+    this.apolloClient
+      .watchQuery({ query: this.GET_LISTINGS })
+      .valueChanges.subscribe((response) => {
+        this.listing_data = response.data as object;
+        console.log(this.listing_data);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ApolloModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            //replace your graphql url http://localhost:3000/graphql
+            uri: 'http://localhost:3000/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
